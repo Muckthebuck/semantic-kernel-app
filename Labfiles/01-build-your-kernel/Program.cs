0ce74ad2c36be07589e4d044aab57e3a768b358b
Mukul Chodhary <mchodhary@microsoft.com>
2026-01-20 22:19:09 +1100

    gitignore and untracking

--- Labfiles/01-build-your-kernel/Program.cs
+++ Labfiles/01-build-your-kernel/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-// Import namespaces
-
-
 string filePath = Path.GetFullPath("appsettings.json");
 var config = new ConfigurationBuilder()
     .AddJsonFile(filePath)
@@ -11,10 +8,16 @@ var config = new ConfigurationBuilder()
 // Set your values in appsettings.json
 string apiKey = config["AZURE_OPENAI_KEY"]!;
 string endpoint = config["AZURE_OPENAI_ENDPOINT"]!;
-string deploymentName = config["DEPLOYMENT_NAME"]!;
+string modelId = config["DEPLOYMENT_NAME"]!;
 
 
 // Create a kernel with Azure OpenAI chat completion
 
 
 // Test the chat completion service
+
+// create kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// build
+Kernel kernel = builder.Build();
